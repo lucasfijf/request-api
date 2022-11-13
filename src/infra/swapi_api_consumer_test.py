8d4993ccd5dd35fada76ac80f8eb961f6a3a8166
Lucas Rangel <lucasfijf@gmail.com>
2022-11-13 13:17:01 -0300

    feat: first interaction

---
+++ src/infra/swapi_api_consumer_test.py
@@ -0,0 +1,8 @@
+from .swapi_api_consumer import SwapiApiConsumer
+
+def test_get_starships():
+    """Testing get_starships method"""
+    swapi_api_consumer = SwapiApiConsumer()
+    response = swapi_api_consumer.get_starships(page=7)
+
+    print(response)
